95226db2cafaea1a47fbbb88ce908297ec6a9c15
Niyati sheth <niyati.radicalloop@gmail.com>
2025-08-26 15:43:40 +0530

    img issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,4 @@
 
-
-export async function generateMetadata() {
-  try {
-    const baseUrl = "https://meta-next-js.vercel.app";
-    const metaData = {
-      title: "TopFrog",
-      keywords:
-        "AI",
-      description:
-        "Discover",
-      metadataBase: new URL(baseUrl),
-      openGraph: {
-        title: "TopFrog",
-        description:
-          "Discover ",
-        type: `article`,
-        locale: `en-US`,
-        images: "/banner.png",
-      },
-    };
-
-    return metaData;
-  } catch (error) {
-    console.log({ error });
-  }
-}
-
 export default function Home() {
   return (
    <div>
